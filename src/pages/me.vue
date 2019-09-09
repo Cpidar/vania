f819a2cb43c802183c9a199cd20dca667ed8f61f
Cpidar <sayeyesepidar@gmail.com>
2019-09-09 19:25:06 +0430

    backup

--- src/pages/me.vue
+++ src/pages/me.vue
@@ -7,13 +7,7 @@
             icon="local_atm"
             label="طول پریود"
           >
-            <q-slider
-              class="dark"
-              v-model="periodLength"
-              :min="2"
-              :max="12"
-              label-always
-            />
+            <q-btn color="primary" @click="changeCycleLength">Pick</q-btn>
           </q-field>
         </q-item-main>
       </q-item>
@@ -53,39 +47,34 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import { model$ } from '../state'
 import { pluck } from 'rxjs/operators'
-import cbtn from '../components/circle-button.vue'
-import { WheelSelector } from '@ionic-native/wheel-selector/ngx'
 
 function range(start: number, end: number) {
   var ans = [];
   for (let i = start; i <= end; i++) {
-    ans.push({ description: `${i}`, id: i });
+    ans.push({ description: `${i}` });
   }
   return ans;
 }
 
-@Component({
-  components: {cbtn}
-})
+@Component({})
 export default class Me extends Vue {
   periodLength = 5;
   cycleLength = 28;
   lock = false;
-  private selector: WheelSelector = new WheelSelector()
 
   wheelSelectorData = {
     cycleLength: range(18, 42),
     bleedingLength: range(3, 7)
   }
 
-  changeCycleLength() {
-    this.selector.show({
+  config = {
       title: "How Many?",
-      items: [ this.wheelSelectorData.cycleLength ],
-    }).then(
-      result => console.log(result),
-      err => console.error(err)
-     )
+      items: [[this.wheelSelectorData.cycleLength]],
+  }
+
+
+  changeCycleLength() {
+    window.SelectorCordovaPlugin.showSelector(this.config, result => console.log(result), err => console.error(err))
   }
 
 }
